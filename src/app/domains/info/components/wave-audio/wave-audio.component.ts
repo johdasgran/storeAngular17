57012469bb76a3fba2c603dc7b8545b1ce060039
Johan Grandas 🌊 <dampig14@gmail.com>
2024-01-02 20:11:43 -0500

    add wave audio

---
+++ src/app/domains/info/components/wave-audio/wave-audio.component.ts
@@ -0,0 +1,34 @@
+import { Component, ElementRef, Input, ViewChild, signal } from '@angular/core';
+
+import WaveSurfer from 'wavesurfer.js';
+
+@Component({
+  selector: 'app-wave-audio',
+  standalone: true,
+  imports: [],
+  templateUrl: './wave-audio.component.html',
+  styleUrl: './wave-audio.component.scss'
+})
+export class WaveAudioComponent {
+
+  @Input() audioUrl!: string;
+  @ViewChild("wave") container!: ElementRef;
+  private ws!: WaveSurfer;
+  isPlaying = signal(false);
+
+  ngAfterViewInit() {
+    this.ws =  WaveSurfer.create({
+      url: this.audioUrl,
+      container: this.container.nativeElement,
+      waveColor: '#328ff0',
+      progressColor: '#1872d0',
+    })
+    this.ws.on('play', ()=> this.isPlaying.set(true))
+    this.ws.on('pause', ()=> this.isPlaying.set(false))
+  }
+
+  toggleAudio() {
+    this.ws.playPause();
+  }
+
+}
